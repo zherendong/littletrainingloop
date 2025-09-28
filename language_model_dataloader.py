initial scaling analysis; split eval config; fix special tokens issue

language_model_dataloader.py
@@ -4,9 +4,7 @@ Data loader for JSONL files.
 
 import glob
 import json
-import logging
 from typing import Any, Callable, Iterable
-from concurrent.futures import ThreadPoolExecutor
 import torch
 import tiktoken
 
@@ -61,9 +59,8 @@ class TokenizedDataLoader(DataProvider[dict[str, Any]]):
         """Create a fresh iterator."""
         for data in self.raw_data_loader.generate():
             text = self.data_to_text(data)
-            tokens = self.tokenizer.encode(text)
+            tokens = self.tokenizer.encode(text, disallowed_special=())
             text_per_token = [self.tokenizer.decode([t]) for t in tokens]
-            # text_per_token = ["" for t in tokens]
             yield {
                 "tokens": tokens,
                 "raw_text": text,
@@ -98,9 +95,7 @@ class BatchedDataLoader(DataProvider[DataItem]):
         self.split = split
         self.name = name
         self.batch_size = (
-            config.batch_size
-            if split == "train"
-            else config.training_config.eval_batch_size
+            config.batch_size if split == "train" else config.eval_config.batch_size
         )
 
     def generate(self) -> Iterable[DataItem]:
@@ -149,9 +144,9 @@ class BatchedDataLoader(DataProvider[DataItem]):
                     if len(tokens) == self.config.sequence_length:
                         break
 
-                assert len(tokens) == self.config.sequence_length, (
-                    f"got {len(tokens)}; expected {self.config.sequence_length} tokens."
-                )
+                assert (
+                    len(tokens) == self.config.sequence_length
+                ), f"got {len(tokens)}; expected {self.config.sequence_length} tokens."
                 assert len(text_per_tokens) == self.config.sequence_length
                 inputs[batch_idx] = torch.tensor(tokens)
                 target_tokens = tokens[1:] + [self.tokenizer.eot_token]

language_model_training.py
@@ -2,6 +2,7 @@
 Language model training.
 """
 
+import argparse
 import os
 import time
 import torch.cuda.nvtx as nvtx
@@ -15,6 +16,7 @@ from training_basics import (
 from language_model_basics import (
     DataItem,
     LanguageModelTrainingConfig,
+    EvalConfig,
 )
 from training_loop import train
 import torch
@@ -78,7 +80,7 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
             config.training_config.num_epochs
             * config.training_config.training_steps_per_epoch
         )
-        switch_step = num_steps * 0.05
+        switch_step = config.warmup_steps or num_steps * 0.05
         linear_warmup = optim.lr_scheduler.LinearLR(
             self.optimizer, start_factor=0.1, end_factor=1.0, total_iters=switch_step
         )
@@ -95,6 +97,7 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
         )
         self.fcounter = flop_counter.FlopCounterMode(depth=4, display=False)
         self.training_flops_total = 0
+        self.num_tokens_total = 0
 
     def num_parameters(self):
         return sum(p.numel() for p in self.model.parameters())
@@ -133,16 +136,19 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
         # detach loss
         loss_numpy = float(loss.detach().cpu().numpy())
 
+        step_time = time.time() - start
         flops_this_step = self.fcounter.get_total_flops()
         self.training_flops_total += flops_this_step
-        step_time = time.time() - start
+        num_tokens_this_step = data.inputs.numel()
+        self.num_tokens_total += num_tokens_this_step
 
         return {
             "loss": loss_numpy,
             "learning_rate": self.optimizer.param_groups[0]["lr"],
-            "training_flops": self.training_flops_total,
+            "training_pflops_total": self.training_flops_total / 1e15,
             "training_tflops_per_second": flops_this_step / step_time / 1e12,
             "training_step_seconds": step_time,
+            "num_tokens": self.num_tokens_total,
         }
 
     def eval(self, data: DataItem) -> Metrics:
@@ -166,15 +172,7 @@ def train_language_model(
     """Train a language model using configuration object"""
     # Create model
     with prng.PRNG(config.seed + 123123):
-        tconfig = TransformerConfig(
-            num_layers=8,
-            num_heads=8,
-            num_heads_kv=4,
-            head_dim=64,
-            mlp_inner_size=2048,
-            embedding_size=512,
-        )
-        model = TransformerModel(config.vocab_size, tconfig)
+        model = TransformerModel(config.vocab_size, config.model_config)
     # Create training state
     with prng.PRNG(config.seed + 234234):
         state = LanguageModelTrainingState(model, config)
@@ -204,7 +202,8 @@ def train_language_model(
     losses = train(
         state,
         train_dataset,
-        config.training_config,
+        config=config.training_config,
+        eval_config=config.eval_config,
         eval_data_providers=eval_datasets,
         neptune_run=neptune_run,
     )
@@ -226,16 +225,29 @@ def run():
     try:
         config = LanguageModelTrainingConfig(
             vocab_size=100277,
-            learning_rate=0.001,
+            warmup_steps=100,
+            learning_rate=0.0005,
             batch_size=64,
             sequence_length=512,
             shuffle_buffer_size=100,
             training_config=TrainingConfig(
                 num_epochs=1,
-                training_steps_per_epoch=5000,
-                eval_every_n_steps=50,
-                eval_steps=10,
+                training_steps_per_epoch=10000,
                 seed=42,
+            ),
+            eval_config=EvalConfig(
+                every_n_steps=50,
+                steps=10,
+                batch_size=64,
+                sequence_length=512,
+            ),
+            model_config=TransformerConfig(
+                num_layers=12,
+                num_heads=8,
+                num_heads_kv=4,
+                head_dim=128,
+                mlp_inner_size=4096,
+                embedding_size=1024,
             ),
         )
         losses = train_language_model(config, neptune_run=neptune_run)
@@ -245,4 +257,9 @@ def run():
 
 
 if __name__ == "__main__":
+
+    # command line args, including name
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
     run()

transformer.py
@@ -30,6 +30,7 @@ class FP32LayerNorm(nn.Module):
             dtype=torch.float32,
         )
 
+    @torch.compile()
     def forward(self, x):
         input_dtype = x.dtype
         x = x.to(torch.float32)
@@ -67,7 +68,7 @@ class MLP(nn.Module):
 def attention_fn(q, k, v, use_flash: bool = True):
     if use_flash:
         try:
-            from flash_attn import flash_attn_func
+            import flash_attn
 
             # # q: (batch_size, seqlen, nheads, headdim)
             batch_size, sequence_length_q, num_heads_kv, q_per_kv, head_dim = q.shape
@@ -77,10 +78,12 @@ def attention_fn(q, k, v, use_flash: bool = True):
             sequence_length_kv = k.shape[1]
             k = k.view(batch_size, sequence_length_kv, num_heads_kv, head_dim)
             v = v.view(batch_size, sequence_length_kv, num_heads_kv, head_dim)
-            return flash_attn_func(q, k, v, causal=True).view(
+            res = flash_attn.flash_attn_func(q, k, v, causal=True)
+            return res.view(
                 batch_size, sequence_length_q, num_heads_kv, q_per_kv, head_dim
             )
         except ImportError:
+            print("Flash attention not available, using slow attention")
             pass
     batch_size, sequence_length_q, num_heads_kv, q_per_kv, head_dim = q.shape
     num_heads_q = num_heads_kv * q_per_kv
@@ -211,7 +214,6 @@ class TransformerModel(nn.Module):
             dtype=torch.bfloat16,
             bias=False,
         )
-        # print number of parameters
         print(
             f"Num non-embedding parameters: {sum(p.numel() for p in self.transformer_blocks.parameters())} parameters"
         )

transformer_test.py
@@ -15,6 +15,9 @@ from transformer import (
     attention_fn,
 )
 
+if torch.cuda.is_available():
+    torch.set_default_device("cuda")
+
 
 def test_mlp():
     """Smoke test for the MLP class"""
@@ -76,27 +79,37 @@ def test_transformer_model():
     )
     vocab_size = 1024
     model = TransformerModel(vocab_size=vocab_size, config=config)
-    x = torch.randint(0, vocab_size, (3, 10))
-    fcounter = flop_counter.FlopCounterMode(depth=4)
-    with fcounter:
+    x = torch.randint(0, vocab_size, (3, 10), dtype=torch.int32)
+    flops = flop_counter.FlopCounterMode(depth=4)
+    with flops:
         y = model(x)
     assert y.shape == (3, 10, vocab_size)
     assert not torch.isnan(y).any()
-    assert fcounter.get_total_flops() == 67461120
-    assert sum(p.numel() for p in model.parameters()) == 1379328
+    assert flops.get_total_flops() == 66846720
+    assert sum(p.numel() for p in model.parameters()) == 1377280
+
+    # now count with backward
+    flops = flop_counter.FlopCounterMode(depth=4)
+    with flops:
+        y = model(x)
+        loss = y.sum()
+        loss.backward()
+    assert flops.get_total_flops() == 200540160
 
 
 def test_flash_nonflash_equivalence():
     """Test that flash and non-flash attention give the same results"""
     if not torch.cuda.is_available():
         return
-    q = torch.normal(mean=0, std=1, size=(1, 3, 2, 4, 32), dtype=torch.bfloat16, device="cuda")
-    k = torch.normal(mean=0, std=1, size=(1, 3, 2, 32), dtype=torch.bfloat16, device="cuda")
-    v = torch.normal(mean=0, std=1, size=(1, 3, 2, 32), dtype=torch.bfloat16, device="cuda")
-    # q_copy = q.clone()
-    # q_copy = q.reshape(1, 3, 2 * 4, 32)
-    # k_copy = k.clone()
-    # v_copy = v.clone()
+    q = torch.normal(
+        mean=0, std=1, size=(1, 3, 2, 4, 32), dtype=torch.bfloat16, device="cuda"
+    )
+    k = torch.normal(
+        mean=0, std=1, size=(1, 3, 2, 32), dtype=torch.bfloat16, device="cuda"
+    )
+    v = torch.normal(
+        mean=0, std=1, size=(1, 3, 2, 32), dtype=torch.bfloat16, device="cuda"
+    )
     y_flash = attention_fn(q, k, v, use_flash=True)
     y_nonflash = attention_fn(q, k, v, use_flash=False)
     torch.testing.assert_close(y_flash, y_nonflash, rtol=1e-2, atol=1e-2)