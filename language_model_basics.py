remote process dataloader; fix a bug in loss computation

language_model_basics.py
@@ -8,6 +8,7 @@ from typing import Any
 
 import torch
 import training_basics
+import numpy as np
 
 # Ignore_index is a magic number for cross entropy loss
 # when a target token is set to this value, we ignore the
@@ -16,10 +17,10 @@ cross_entropy_ignore_index = -100
 
 
 @dataclasses.dataclass
-class DataItem:
-    inputs: torch.Tensor
-    targets: torch.Tensor
-    loss_mask: torch.Tensor
+class LMData:
+    inputs: np.ndarray
+    targets: np.ndarray
+    loss_mask: np.ndarray
     metadata: dict[str, Any] = dataclasses.field(default_factory=dict)
 
 

language_model_dataloader.py
@@ -4,15 +4,16 @@ Data loader for JSONL files.
 
 import glob
 import json
-from typing import Any, Callable, Iterable
+from typing import Any, Callable, Iterable, TypeVar
 import time
 
-import torch
+import numpy as np
 import tiktoken
 
 from training_loop import DataProvider
-from language_model_basics import DataItem, LanguageModelTrainingConfig
+from language_model_basics import LMData, LanguageModelTrainingConfig
 import prng
+import multiprocess_iterable
 
 
 def default_tokenizer():
@@ -74,7 +75,7 @@ class TokenizedDataLoader(DataProvider[dict[str, Any]]):
         return f"Tokenized dataset ({self.raw_data_loader.get_name()})"
 
 
-class BatchedDataLoader(DataProvider[DataItem]):
+class BatchedDataLoader(DataProvider[LMData]):
     """Data loader creating batches.
 
     Tokenizes text data on the fly and returns batches of token ids.
@@ -99,7 +100,7 @@ class BatchedDataLoader(DataProvider[DataItem]):
         self.split = split
         self.name = name
 
-    def generate(self) -> Iterable[DataItem]:
+    def generate(self) -> Iterable[LMData]:
         """Create a fresh iterator."""
         global_data_stream = iter(self.tokenized_data_loader.generate())
         rest_data_per_batch = [None] * self.batch_size
@@ -107,9 +108,9 @@ class BatchedDataLoader(DataProvider[DataItem]):
         while True:
             batch_start_time = time.time()
             shape = (self.batch_size, self.sequence_length)
-            inputs = torch.zeros(shape, dtype=torch.int32)
-            targets = torch.zeros(shape, dtype=torch.int32)
-            loss_mask = torch.ones(shape, dtype=torch.float32)
+            inputs = np.zeros(shape, dtype=np.int32)
+            targets = np.zeros(shape, dtype=np.int32)
+            loss_mask = np.ones(shape, dtype=np.float32)
             metadata = {"text_per_tokens": []}
 
             def continued_data_stream(rest_data) -> Iterable[dict[str, Any]]:
@@ -150,15 +151,15 @@ class BatchedDataLoader(DataProvider[DataItem]):
                     len(tokens) == self.sequence_length
                 ), f"got {len(tokens)}; expected {self.sequence_length} tokens."
                 assert len(text_per_tokens) == self.sequence_length
-                inputs[batch_idx] = torch.tensor(tokens, dtype=torch.int32)
+                inputs[batch_idx] = tokens
                 target_tokens = tokens[1:] + [self.tokenizer.eot_token]
-                targets[batch_idx] = torch.tensor(target_tokens, dtype=torch.int32)
+                targets[batch_idx] = target_tokens
                 loss_mask[batch_idx, -1] = 0.0  # mask out the EOT token
                 metadata["text_per_tokens"].append(text_per_tokens)
 
             batch_end_time = time.time()
             print(f"Batch time: {batch_end_time - batch_start_time}")
-            yield DataItem(
+            yield LMData(
                 inputs,
                 targets,
                 loss_mask,
@@ -170,3 +171,34 @@ class BatchedDataLoader(DataProvider[DataItem]):
         if self.name:
             return self.name
         return f"Batched LM dataset ({self.batch_size=}, {self.tokenized_data_loader.get_name()})"
+
+
+T = TypeVar("T")
+
+
+class MultiProcessDataloader(DataProvider[T]):
+    """Wrap a dataloader to run it in a separate process."""
+
+    def __init__(
+        self,
+        dataloader_factory: Callable[..., Iterable[T]],
+        kwargs: dict[str, Any],
+        prefetch: int,
+        name: str | None = None,
+    ):
+        self.dataloader_factory = dataloader_factory
+        self.kwargs = kwargs
+        self.prefetch = prefetch
+        self.name = name
+
+    def generate(self) -> Iterable[T]:
+        """Create a fresh iterator."""
+        yield from multiprocess_iterable.GeneratorProcess(
+            self.dataloader_factory, self.kwargs, prefetch=self.prefetch
+        )
+
+    def get_name(self) -> str:
+        """Name of the dataset"""
+        if self.name:
+            return self.name
+        return f"MultiProcessDataloader({self.dataloader_factory.__name__})"

language_model_training.py
@@ -16,7 +16,7 @@ from training_basics import (
     Metrics,
 )
 from language_model_basics import (
-    DataItem,
+    LMData,
     LanguageModelTrainingConfig,
     EvalConfig,
 )
@@ -85,7 +85,7 @@ class DummyLanguageModel(language_model_basics.LanguageModel):
         return self.num_parameters() - self.num_embedding_parameters()
 
 
-class LanguageModelTrainingState(TrainingState[DataItem]):
+class LanguageModelTrainingState(TrainingState[LMData]):
     """Training state for language model"""
 
     def __init__(
@@ -129,17 +129,25 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
             6 * self.model.num_non_embedding_parameters() * batch_size * sequence_length
         )
 
-    def step(self, data: DataItem) -> Metrics:
+    def step(self, data: LMData) -> Metrics:
+        inputs = torch.tensor(data.inputs, dtype=torch.int32)
+        targets = torch.tensor(data.targets, dtype=torch.long)
+        loss_mask = torch.tensor(data.loss_mask, dtype=torch.float32)
+        assert inputs.shape == targets.shape == loss_mask.shape
+        assert inputs.shape == (
+            self.config.batch_size,
+            self.config.sequence_length,
+        )
 
         start = time.time()
         targets = torch.where(
-            data.loss_mask == 0.0,
+            loss_mask == 0.0,
             _cross_entropy_ignore_index,
-            data.targets,
+            targets,
         )
 
         with nvtx.range("train_step", color="blue"):
-            loss = self.model.compute_loss(data.inputs, targets)
+            loss = self.model.compute_loss(inputs, targets)
 
             # Backward pass
             self.optimizer.zero_grad()
@@ -156,7 +164,7 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
         step_time = time.time() - start
         flops_this_step = self.flops_per_step(data.inputs.shape)  # type: ignore
         self.training_flops_total += flops_this_step
-        self.num_tokens_seen += data.inputs.numel()
+        self.num_tokens_seen += inputs.numel()
 
         return {
             "loss": loss_numpy,
@@ -167,14 +175,21 @@ class LanguageModelTrainingState(TrainingState[DataItem]):
             "num_tokens": self.num_tokens_seen,
         }
 
-    def eval(self, data: DataItem) -> Metrics:
+    def eval(self, data: LMData) -> Metrics:
+        inputs = torch.tensor(data.inputs, dtype=torch.int32)
+        targets = torch.tensor(data.targets, dtype=torch.long)
+        loss_mask = torch.tensor(data.loss_mask, dtype=torch.float32)
+        assert inputs.shape == (
+            self.config.eval_config.batch_size,
+            self.config.eval_config.sequence_length,
+        )
         with torch.no_grad():
             targets = torch.where(
-                data.loss_mask == 0.0,
+                loss_mask == 0.0,
                 _cross_entropy_ignore_index,
-                data.targets,
+                targets,
             )
-            loss = self.model.compute_loss(data.inputs, targets)
+            loss = self.model.compute_loss(inputs, targets)
             loss = float(loss.to(torch.float32).detach().cpu().numpy())
         return {"loss": loss}
 
@@ -226,7 +241,11 @@ def train_language_model(
             stackv2_dataloader.create_stackv2_dataloader(config, split="validation")
         ]
     elif dataset == "slimpajama":
-        train_dataset = slimpajama_dataloader.create_slimpajama_dataloader(config)
+        train_dataset = (
+            slimpajama_dataloader.create_slimpajama_dataloader_in_separate_process(
+                config
+            )
+        )
         eval_datasets = [
             slimpajama_dataloader.create_slimpajama_dataloader(
                 config, split="validation"
@@ -273,7 +292,7 @@ def run(
     config = LanguageModelTrainingConfig(
         vocab_size=100277,
         warmup_steps=100,
-        learning_rate=0.0005,
+        learning_rate=0.001,
         batch_size=256,
         sequence_length=512,
         shuffle_buffer_size=100,

transformer.py
@@ -238,7 +238,9 @@ class TransformerBlock(nn.Module):
 class TransformerModel(language_model_basics.LanguageModel):
     """Simple transformer model."""
 
-    def __init__(self, vocab_size: int, config: TransformerConfig, dtype=torch.float32):
+    def __init__(
+        self, vocab_size: int, config: TransformerConfig, dtype=torch.bfloat16
+    ):
         super(TransformerModel, self).__init__()
 
         if dtype == torch.float32:
@@ -305,16 +307,18 @@ class TransformerModel(language_model_basics.LanguageModel):
         x = self.embedding(x)
         x = self.transformer_blocks(x)
         x = self.final_norm(x)
+        # don't apply the output projection, as it's handled differently in
+        # compute_loss and forward.
         return x
 
     def compute_loss(self, inputs: torch.Tensor, targets: torch.Tensor):
+        assert targets.dtype == torch.long
         final_emb = self._forward_opt(inputs)
 
         # flatten batch and sequence length for cross entropy
         final_emb = final_emb.view(-1, self.dim)
-        targets = targets.view(-1).to(torch.long)
+        targets = targets.view(-1)
 
-        targets = targets.to(self.dtype)
         weights = self.output_projection.weight.to(self.dtype)
 
         loss = cut_cross_entropy.linear_cross_entropy(