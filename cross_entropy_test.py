Fixed bf16 training. Move to split loss computation.

cross_entropy_test.py
@@ -1,15 +1,17 @@
 """
-Test script for the cut_cross_entropy.py module.
+Test script for cross_entropy.py.
 
-"Cut Your Losses in Large-Vocabulary Language Models"
 
-https://arxiv.org/abs/2411.09009
+Also includes tests for cut cross entropy (linear cross entropy).
+
+"Cut Your Losses in Large-Vocabulary Language Models" https://arxiv.org/abs/2411.09009
 """
 
 import pytest
+import torch
 
 from cut_cross_entropy import linear_cross_entropy
-import torch
+import cross_entropy
 
 
 # make cuda default
@@ -29,7 +31,13 @@ def test_linear_cross_entropy_shape():
 
 
 def compare_linear_cross_entropy(embeddings, projection_weights, targets):
-    loss_linear = linear_cross_entropy(embeddings, projection_weights, targets)
+    loss_linear = linear_cross_entropy(
+        embeddings,
+        projection_weights,
+        targets,
+        accum_e_fp32=True,
+        accum_c_fp32=True,
+    )
     loss_linear = loss_linear.to(torch.float32)
 
     logits = embeddings @ projection_weights.t()
@@ -37,7 +45,7 @@ def compare_linear_cross_entropy(embeddings, projection_weights, targets):
     loss_cross_entropy = loss_cross_entropy.to(torch.float32)
 
     print(f"{loss_linear=}, {loss_cross_entropy=}")
-    torch.testing.assert_close(loss_linear, loss_cross_entropy, rtol=2e-3, atol=2e-3)
+    torch.testing.assert_close(loss_linear, loss_cross_entropy, rtol=5e-3, atol=5e-3)
 
 
 def test_linear_cross_entropy_equal_to_cross_entropy():
@@ -58,3 +66,21 @@ def test_equivalence_with_random_data(dtype: torch.dtype):
     targets = torch.randint(0, 1000, (512,), dtype=torch.long)
 
     compare_linear_cross_entropy(embeddings, projection_weights, targets)
+
+
+def test_segmented_cross_entropy_equivalence(dtype=torch.float32):
+    embeddings = torch.randn(
+        29000, 128, device="cuda", dtype=dtype
+    )  # 512 samples, 128 dimensions
+    projection_weights = torch.randn(
+        1000, 128, device="cuda", dtype=dtype
+    )  # 1000 classes
+    targets = torch.randint(0, 1000, (29000,), dtype=torch.long, device="cuda")
+
+    loss1 = cross_entropy.cross_entropy_with_logits_by_segment(
+        embeddings, projection_weights, targets
+    )
+    loss2 = cross_entropy._cross_entropy_with_logits(
+        embeddings, projection_weights, targets
+    )
+    torch.testing.assert_close(loss1, loss2, rtol=5e-3, atol=5e-3)

language_model_training.py
@@ -33,13 +33,16 @@ import language_model_basics
 import neptune
 import transformer
 import model_configs.chinchilla  # noqa: F401
-import cut_cross_entropy
+import cross_entropy
 
+# import bf16_fused_adam
+import optimi  # for 16-bit optimizers
 
-# Ignore_index is a magic number for cross entropy loss
-# when a target token is set to this value, we ignore the
-# loss for this token.
-_cross_entropy_ignore_index = -100
+os.environ["TORCHINDUCTOR_CACHE_DIR"] = "/tmp/torchinductor_cache"
+os.environ["TORCHINDUCTOR_FX_GRAPH_CACHE"] = "1"
+os.environ["TORCHINDUCTOR_AUTOGRAD_CACHE"] = "1"
+
+torch.set_float32_matmul_precision("high")  # enable use TF32 to enable tensor cores
 
 
 class DummyLanguageModel(language_model_basics.LanguageModel):
@@ -97,7 +100,17 @@ class LanguageModelTrainingState(TrainingState[LMData]):
         self.config = config
 
         with prng.PRNG(config.seed + 345345):
-            self.optimizer = optim.AdamW(model.parameters(), lr=config.learning_rate)
+            # self.optimizer = optim.AdamW(model.parameters(), lr=config.learning_rate)
+            # https://optimi.benjaminwarner.dev/kahan_summation/
+            self.optimizer = optimi.AdamW(
+                model.parameters(),
+                lr=config.learning_rate,
+                # eps 1e-8 is the default in pytorch AdamW, and
+                # 1e-7 in in optimi.
+                eps=1e-7,
+                # weight_decay=0.01,
+                # betas=(0.9, 0.999),
+            )
 
         # linear learning rate schedule with warmup
         assert config.training_config.training_steps_per_epoch is not None
@@ -142,7 +155,7 @@ class LanguageModelTrainingState(TrainingState[LMData]):
         start = time.time()
         targets = torch.where(
             loss_mask == 0.0,
-            _cross_entropy_ignore_index,
+            cross_entropy.cross_entropy_ignore_index,
             targets,
         )
 
@@ -186,7 +199,7 @@ class LanguageModelTrainingState(TrainingState[LMData]):
         with torch.no_grad():
             targets = torch.where(
                 loss_mask == 0.0,
-                _cross_entropy_ignore_index,
+                cross_entropy.cross_entropy_ignore_index,
                 targets,
             )
             loss = self.model.compute_loss(inputs, targets)

training_loop.py
@@ -6,9 +6,11 @@ Design goals:
 - generate a clean list of functions to implement
 """
 
-import dataclasses
+import time
 from collections import defaultdict
 from typing import Sequence
+
+import torch
 
 from training_basics import (
     TrainingConfig,
@@ -96,6 +98,17 @@ def train(
                 )
             if idx >= config.training_steps_per_epoch:
                 break
+            if idx == 5:
+                torch.cuda.synchronize()
+                torch.cuda.reset_peak_memory_stats()
+                torch.cuda.empty_cache()
+                torch.cuda.synchronize()
+                print(torch.cuda.memory_summary())
+                # snap = (
+                #     torch.cuda.memory_snapshot()
+                # )  # JSON-like: blocks, sizes, “active” flags
+                # print(snap)
+
             metrics = state.step(data)
             process_metrics(metrics, neptune_run=neptune_run, step=idx, mode="train")
 

transformer.py
@@ -8,11 +8,11 @@ import math
 from torch.utils import checkpoint
 import torch
 import torch.nn as nn
-import attention
 import torchtune
 
+import attention
+import cross_entropy
 import language_model_basics
-import cut_cross_entropy
 
 
 @dataclasses.dataclass(frozen=True)
@@ -235,24 +235,38 @@ class TransformerBlock(nn.Module):
         return self._forward(x)
 
 
+def mem_gb():
+    s = torch.cuda.memory_stats()
+    alloc = s["allocated_bytes.all.current"] / 1e9
+    peak = s["allocated_bytes.all.peak"] / 1e9
+    resv = s["reserved_bytes.all.current"] / 1e9
+    frag = resv - alloc
+    return alloc, peak, resv, frag
+
+
 class TransformerModel(language_model_basics.LanguageModel):
     """Simple transformer model."""
 
     def __init__(
-        self, vocab_size: int, config: TransformerConfig, dtype=torch.bfloat16
+        self,
+        vocab_size: int,
+        config: TransformerConfig,
+        params_dtype=torch.bfloat16,
+        activation_dtype=torch.bfloat16,
     ):
         super(TransformerModel, self).__init__()
-
-        if dtype == torch.float32:
-            print("Using float32")
-            torch.set_float32_matmul_precision(
-                "high"
-            )  # enable use TF32 to enable tensor cores
         self.vocab_size = vocab_size
         self.dim = config.embedding_size
         self.config = config
-        self.dtype = dtype
-        self.embedding = nn.Embedding(vocab_size, self.dim, dtype=dtype)
+        self.params_dtype = params_dtype
+        self.activation_dtype = activation_dtype
+        self.embedding = nn.Embedding(
+            vocab_size,
+            self.dim,
+            # Always float32 for embedding, as recommended
+            # for optimal quality.
+            dtype=torch.float32,
+        )
         self.transformer_blocks = nn.Sequential(
             *[
                 TransformerBlock(
@@ -261,7 +275,7 @@ class TransformerModel(language_model_basics.LanguageModel):
                     lambda: MLP(
                         self.dim,
                         inner_size=config.mlp_inner_size,
-                        dtype=dtype,
+                        dtype=params_dtype,
                     ),
                     lambda: SelfAttention(
                         self.dim,
@@ -269,7 +283,7 @@ class TransformerModel(language_model_basics.LanguageModel):
                         config.num_heads_kv,
                         config.head_dim,
                         use_flash_attention=config.use_flash_attention,
-                        dtype=dtype,
+                        dtype=params_dtype,
                     ),
                 )
                 for i in range(config.num_layers)
@@ -281,7 +295,7 @@ class TransformerModel(language_model_basics.LanguageModel):
         self.output_projection = nn.Linear(
             self.dim,
             vocab_size,
-            dtype=dtype,
+            dtype=self.params_dtype,
             bias=False,
         )
         print(
@@ -304,7 +318,7 @@ class TransformerModel(language_model_basics.LanguageModel):
 
         Use get_output_projection_weights to get the weights to compute the logits.
         """
-        x = self.embedding(x)
+        x = self.embedding(x).to(self.activation_dtype)
         x = self.transformer_blocks(x)
         x = self.final_norm(x)
         # don't apply the output projection, as it's handled differently in
@@ -312,6 +326,10 @@ class TransformerModel(language_model_basics.LanguageModel):
         return x
 
     def compute_loss(self, inputs: torch.Tensor, targets: torch.Tensor):
+
+        alloc, peak, resv, frag = mem_gb()
+        print(f"compute_loss: {alloc=}, {peak=}, {resv=}, {frag=}")
+
         assert targets.dtype == torch.long
         final_emb = self._forward_opt(inputs)
 
@@ -319,16 +337,20 @@ class TransformerModel(language_model_basics.LanguageModel):
         final_emb = final_emb.view(-1, self.dim)
         targets = targets.view(-1)
 
-        weights = self.output_projection.weight.to(self.dtype)
+        weights = self.output_projection.weight
 
-        loss = cut_cross_entropy.linear_cross_entropy(
-            final_emb,
-            weights,
-            targets,
-            ignore_index=language_model_basics.cross_entropy_ignore_index,
-            filter_eps=torch.finfo(torch.float32).eps,
-            accum_e_fp32=True,
-            accum_c_fp32=True,
+        # loss = cut_cross_entropy.linear_cross_entropy(
+        #     final_emb,
+        #     weights,
+        #     targets,
+        #     ignore_index=cross_entropy.cross_entropy_ignore_index,
+        #     filter_eps=torch.finfo(torch.float32).eps,
+        #     accum_e_fp32=True,
+        #     accum_c_fp32=True,
+        # )
+        # return loss
+        loss = cross_entropy.cross_entropy_with_logits_by_segment(
+            final_emb.clone(), weights, targets
         )
         return loss
 