4e46d2b0b0f50c11b9dae529d940d266c5765f33
Zheren Dong <dongzheren@gmail.com>
2025-11-06 00:34:32 -0800

    update

--- run_your_experiment.sh
+++ run_your_experiment.sh
@@ -38,7 +38,7 @@ python run_custom_experiment.py --model_size 117m --neptune_tags silu-init-zhere
 
 echo ""
 
-python run_custom_experiment.py --model_size 117m --neptune_tags silu-init-zheren --learning_rate 0.002 --weight_decay 0.1 --warmup_steps 500 --chinchilla_factor 1.0 --no_depth_scaling --description "Training chinchilla-117m with lr=0.002, wd=0.1, warmup=500, proper_init=True, depth_scaling=True. This is to compare with the previous run TRAIN-830."
+python run_custom_experiment.py --model_size 117m --neptune_tags silu-init-zheren --learning_rate 0.002 --weight_decay 0.1 --warmup_steps 500 --chinchilla_factor 1.0 --no_depth_scaling --description "Training chinchilla-117m with lr=0.002, wd=0.1, warmup=500, proper_init=True, depth_scaling=False. This is to compare with the previous run TRAIN-830."
 
 echo ""
 
