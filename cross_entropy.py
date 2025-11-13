fix loss computation issue

cross_entropy.py
@@ -43,13 +43,18 @@ def cross_entropy_with_logits_by_segment(embeddings, weights, targets):
     batch_size = embeddings.shape[0]
     segment_size = 4096 * 2
     num_segments = (batch_size + segment_size - 1) // segment_size
-    losses = torch.zeros(num_segments, device=embeddings.device, dtype=torch.float32)
+    total_loss = 0.0
     for i in range(num_segments):
         start = i * segment_size
         end = min((i + 1) * segment_size, batch_size)
         segment_embeddings = embeddings[start:end]
+        this_segment_size = segment_embeddings.shape[0]
         segment_targets = targets[start:end]
-        losses[i] = _cross_entropy_with_logits_checkpointed(
+        loss = _cross_entropy_with_logits_checkpointed(
             segment_embeddings, weights, segment_targets
         )
-    return losses.mean()
+        if this_segment_size < segment_size:
+            loss *= this_segment_size / segment_size
+        total_loss += loss
+
+    return total_loss / num_segments

spelling_bee_embeddings.py
@@ -94,12 +94,6 @@ class SpellingBeeEmbedding(nn.Module):
             self.max_characters_per_token,
             self.embedding_dim,
         )
-        if torch.isnan(embeddings).any():
-            # determine which character is NaN
-            for char_idx in range(256):
-                if torch.isnan(self.character_embedding.weight[char_idx]).any():
-                    print(f"NaN in character embedding for char {char_idx}")
-            raise ValueError("NaN in character embeddings")
         return embeddings
 
     def _apply_rotary(self, embeddings: torch.Tensor) -> torch.Tensor:
@@ -119,16 +113,12 @@ class SpellingBeeEmbedding(nn.Module):
         """input is a tensor of shape (batch_size, sequence_length) of token ids."""
         embeddings = self.get_character_embeddings(input)
         embeddings = self._apply_rotary(embeddings)
-        if torch.isnan(embeddings).any():
-            raise ValueError("NaN in character embeddings")
         embeddings = embeddings.sum(dim=-2)
-        if torch.isnan(embeddings).any():
-            raise ValueError("NaN in character embeddings")
         if self.separate_token_embedding:
             token_embeddings = self.token_embedding(input)
             embeddings += token_embeddings
-        if torch.isnan(embeddings).any():
-            raise ValueError("NaN in character embeddings")
+        # if torch.isnan(embeddings).any():
+        #     raise ValueError("NaN in character embeddings")
         # print(
         #     f"Character embeddings: mean {embeddings.mean()}, std {embeddings.std()}, max {embeddings.max()}, min {embeddings.min()}"
         # )

transformer.py
@@ -615,10 +615,10 @@ class TransformerModel(language_model_basics.LanguageModel):
             f"Num non-embedding parameters: {self.num_non_embedding_parameters()} parameters"
         )
 
-        # self._forward_opt = torch.compile(
-        #     self._forward, mode="max-autotune", fullgraph=True
-        # )
-        self._forward_opt = self._forward
+        self._forward_opt = torch.compile(
+            self._forward, mode="max-autotune", fullgraph=True
+        )
+        # self._forward_opt = self._forward
 
         if config.zheren_init:
             self.init_weights()
@@ -690,7 +690,6 @@ class TransformerModel(language_model_basics.LanguageModel):
 
     def compute_loss(self, inputs: torch.Tensor, targets: torch.Tensor):
         assert targets.dtype == torch.long
-        torch.compiler.cudagraph_mark_step_begin()
         final_emb = self._forward_opt(inputs)
 
         emb_dim = final_emb.shape[-1]